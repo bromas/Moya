c9d7524dee0924168042bc8fb178d47f57312dff
Brian Thomas <atomos86@live.com>
2015-05-28 16:47:57 -0400

    adds workspace and exposes framework for carthage

---
+++ Moya-iOS/Moya-iOS.h
@@ -0,0 +1,19 @@
+//
+//  Moya-iOS.h
+//  Moya-iOS
+//
+//  Created by Brian Thomas on 5/29/15.
+//  Copyright (c) 2015 Ash Furrow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Moya-iOS.
+FOUNDATION_EXPORT double Moya_iOSVersionNumber;
+
+//! Project version string for Moya-iOS.
+FOUNDATION_EXPORT const unsigned char Moya_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Moya_iOS/PublicHeader.h>
+
+
