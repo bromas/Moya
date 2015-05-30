12fce8a7344b6a192d06b6eb7317c0eae5eee7a0
Brian Thomas <atomos86@live.com>
2015-05-30 17:41:45 -0400

    Removes demo and adds framework for carthage

---
+++ Moya-OSX/Moya-OSX.h
@@ -0,0 +1,19 @@
+//
+//  Moya-OSX.h
+//  Moya-OSX
+//
+//  Created by Brian Thomas on 5/30/15.
+//  Copyright (c) 2015 Ash Furrow. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Moya-OSX.
+FOUNDATION_EXPORT double Moya_OSXVersionNumber;
+
+//! Project version string for Moya-OSX.
+FOUNDATION_EXPORT const unsigned char Moya_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Moya_OSX/PublicHeader.h>
+
+
